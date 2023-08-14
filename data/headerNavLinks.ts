5ddde90eedb47503c02d658e3253d11288cc6635
romrosmay <99548008+romrosmay@users.noreply.github.com>
2023-08-14 21:07:43 +0200

    feat(content): canvis en el aboutme

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,7 +1,4 @@
 export let headerNavLinks = [
-  { href: '/blog', titleKey: 'menu_blog' },
-  { href: '/snippets', titleKey: 'menu_receptes' },
-  { href: '/projects', titleKey: 'menu_projectes' },
   { href: '/about', titleKey: 'menu_sobremi' },
   { href: '/resume', titleKey: 'menu_curriculum' },
 ]
