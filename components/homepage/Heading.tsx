f2692616a35a6579d08a0e207c4d4b3c4cb2d0fe
romrosmay <99548008+romrosmay@users.noreply.github.com>
2023-08-14 17:50:54 +0200

    feat(index): canvis ene le gradient del titol principal

--- components/homepage/Heading.tsx
+++ components/homepage/Heading.tsx
@@ -11,99 +11,106 @@ export function Heading() {
   const [isAnimated3, setIsAnimated3] = useState(false)
 
   useEffect(() => {
-    let timeoutIds = [] // To store the IDs of the timeouts
+    let timeoutIds = []
 
-    const cycleAnimations = () => {
-      timeoutIds.push(
-        setTimeout(() => {
-          setIsAnimated2(true)
-          timeoutIds.push(
-            setTimeout(() => {
-              setIsAnimated1(false)
-              timeoutIds.push(
-                setTimeout(() => {
-                  setIsAnimated3(true)
-                  timeoutIds.push(
-                    setTimeout(() => {
-                      setIsAnimated2(false)
-                      timeoutIds.push(
-                        setTimeout(() => {
-                          setIsAnimated1(true)
-                          timeoutIds.push(
-                            setTimeout(() => {
-                              setIsAnimated3(false)
-                              timeoutIds.push(setTimeout(cycleAnimations, 2000)) // Recurse after the full cycle.
-                            }, 2000)
-                          )
-                        }, 2000)
-                      )
-                    }, 2000)
-                  )
-                }, 2000)
-              )
-            }, 2000)
-          )
-        })
-      )
+    const addTimeout = (callback, delay) => {
+      timeoutIds.push(setTimeout(callback, delay))
     }
 
-    cycleAnimations() // Kick off the animation cycle
+    const cycleAnimations = () => {
+      let delay = 0
+      actions.forEach((action) => {
+        addTimeout(action, delay)
+        delay += 1500
+      })
+    }
+
+    const actions = [
+      () => setIsAnimated2(true),
+      () => setIsAnimated1(false),
+      () => setIsAnimated3(true),
+      () => setIsAnimated2(false),
+      () => setIsAnimated1(true),
+      () => setIsAnimated3(false),
+      cycleAnimations,
+    ]
+
+    cycleAnimations()
 
     return () => {
-      // Clean up all timeouts that we created
       timeoutIds.forEach((id) => clearTimeout(id))
     }
   }, [])
 
-  const commonClasses = 'inline-block subpixel-antialiased'
-  const commonBigTextClasses = 'text-5xl md:text-7xl lg:text-9xl'
-  const tailwindClassName = `text-3xl md:text-4xl lg:text-5xl  ${commonClasses} ${
-    isAnimated1 && 'name-gradient'
-  }`
-  const tailwindClassTech = `${commonBigTextClasses} ${commonClasses} ${
-    isAnimated2 && 'first-word-gradient'
-  }`
-  const tailwindClassLead = `${commonBigTextClasses} ${commonClasses} ${
-    isAnimated3 && 'second-word-gradient'
-  }`
-  const tailwindClassDeveloper = `text-2xl md:text-3xl lg:text-4xl ${commonClasses}}`
-
   return (
-    <>
-      <div className="grid grid-cols-1 gap-4 max-w-2xl">
-        <div className="mb-0   flex items-center text-gray-700 dark:text-gray-200">
-          <LocationPin className="h-4 w-4 lg:h-5 lg:w-5 xl:h-6 xl:w-6" />
-          <p className="px-2">Barcelona, Catalunya</p>
-        </div>
-        <h1 className="mb-0 font-bold font-poppins">
-          <span className={tailwindClassName}> Roman Roset</span>
-        </h1>
-        <div className="grid grid-cols-2 gap-4">
-          <h1 className={`mb-5 font-bold font-poppins`}>
-            <span className={tailwindClassTech}> Tech </span>
-            <br></br>
-            <span className={tailwindClassLead}> Lead </span>
-          </h1>
-          <div className="flex justify-center items-center md:justify-end md:items-end">
-            <Image
-              src={'/static/images/logo.png'}
-              alt={t('avatarDescription')}
-              height={240} // Reduït a la meitat
-              width={240} // Reduït a la meitat
-              className="shadow-profile_img mb-8"
-              style={{
-                objectPosition: '100% 0%',
-                objectFit: 'cover',
-              }}
-            />
-          </div>
-        </div>
-        <h1 className={`mb-4 font-bold font-poppins ${tailwindClassDeveloper}`}>
-          IT project and process manager <br />
-          Full Stack Developer <br />
-          <span>Senior Software Engineer</span>
-        </h1>
-      </div>
-    </>
+    <div className="grid grid-cols-1 gap-4 max-w-2xl">
+      <LocationInfo />
+      <NameHeader isAnimated1={isAnimated1} />
+      <RoleImages isAnimated2={isAnimated2} isAnimated3={isAnimated3} />
+      <RolesHeader />
+    </div>
   )
 }
+
+const LocationInfo = () => (
+  <div className="mb-0 flex items-center text-gray-700 dark:text-gray-200">
+    <LocationPin className="h-4 w-4 lg:h-5 lg:w-5 xl:h-6 xl:w-6" />
+    <p className="px-2">Barcelona, Catalunya</p>
+  </div>
+)
+
+const NameHeader = ({ isAnimated1 }) => (
+  <h1 className="mb-0 font-bold font-poppins">
+    <span
+      className={`text-3xl md:text-4xl lg:text-5xl inline-block subpixel-antialiased ${
+        isAnimated1 && 'name-gradient'
+      }`}
+    >
+      {' '}
+      Roman Roset
+    </span>
+  </h1>
+)
+
+const RoleImages = ({ isAnimated2, isAnimated3 }) => (
+  <div className="grid grid-cols-2 gap-4">
+    <h1 className="mb-5 font-bold font-poppins">
+      <span
+        className={`text-8xl md:text-8xl lg:text-9xl inline-block subpixel-antialiased ${
+          isAnimated2 && 'first-word-gradient'
+        }`}
+      >
+        {' '}
+        Tech{' '}
+      </span>
+      <br />
+      <span
+        className={`text-8xl md:text-8xl lg:text-9xl inline-block subpixel-antialiased ${
+          isAnimated3 && 'second-word-gradient'
+        }`}
+      >
+        {' '}
+        Lead{' '}
+      </span>
+    </h1>
+    <div className="flex justify-start md:justify-end items-center">
+      <div className="w-48 h-48 md:w-60 md:h-60 lg:w-72 lg:h-72 relative">
+        <Image
+          src="/static/images/logo.png"
+          alt="Descripció de l'avatar"
+          layout="fill"
+          objectFit="cover"
+          className="shadow-profile_img mb-8 absolute top-0 left-0"
+        />
+      </div>
+    </div>
+  </div>
+)
+
+const RolesHeader = () => (
+  <h1 className="mb-12 font-bold font-poppins text-2xl md:text-3xl lg:text-4xl inline-block subpixel-antialiased">
+    IT project and process manager <br />
+    Full Stack Developer <br />
+    <span>Senior Software Engineer</span>
+  </h1>
+)
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
 import Image from 'next/image'
-import { useEffect, useState } from 'react'
 import { PageSeo } from 'components/SEO'
 import { Heading } from '~/components/homepage/Heading'
 import { getAllFilesFrontMatter } from '~/libs/mdx'
@@ -8,39 +7,39 @@ import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 import EmailButton from '~/components/homepage/EmailButton'
 
 export async function getStaticProps({ locale }) {
-  let posts = getAllFilesFrontMatter(`${locale}/blog`)
+  const posts = getAllFilesFrontMatter(`${locale}/blog`)
+  const translations = await serverSideTranslations(locale, ['common'])
 
-  // Afegeix la traducció
   return {
     props: {
       posts,
-      ...(await serverSideTranslations(locale, ['common'])), // Aquí estem assumint que el nom del teu fitxer de traducció és 'common'
+      ...translations,
     },
   }
 }
 
 export default function Home({ posts }) {
-  const { t } = useTranslation('common') // utilitza 'common' si els teus strings estan a common.ts o canvia-ho pel nom adequat
+  const { t } = useTranslation('common')
 
   return (
     <>
       <PageSeo title={t('siteMetadata.title')} description={t('siteMetadata.description')} />
-      <div className="space-y-16 md:my-4 md:space-y-5  md:pt-6 xl:grid xl:grid-cols-3">
-        <div className="md:pr-8 xl:col-span-2">
+      <div className="md:pt-6 xl:grid xl:grid-cols-3 space-y-16 md:space-y-5">
+        <div className="md:pr-8 xl:col-span-2 space-y-16 md:space-y-0">
+          <Heading />
           <div className="text-lg leading-8 text-gray-600 dark:text-gray-400">
-            <Heading />
-            <p className="my-0 md:my-16 md:mt-8  md:mb-0 text-neutral xl:leading-loose">
+            <p className="text-neutral xl:leading-loose">
               Professional amb més de 20 anys en tecnologia, expert en tecnologies frontend i
               backend, amb experiència liderant processos, gestionant projectes tecnològics i
               desenvolupant solucions de programari en startups, fundacions privades, institucions
               educatives i centres de recerca.
             </p>
-            <p className="my-0 md:my-16 md:mt-8  md:mb-0 text-neutral xl:leading-loose">
+            <p className="mt-8 text-neutral xl:leading-loose">
               Actualment treballo a la Fundacio .cat{' '}
             </p>
           </div>
         </div>
-        <div className="flex flex-col items-center justify-start h-full pb-6 mt-16">
+        <div className="mt-16 pb-6 h-full flex flex-col items-center justify-start">
           <EmailButton email="roman.roset@email.com" />
         </div>
       </div>
